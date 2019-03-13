0b6557751ecaf13d101ee74f3933c69501503deb
emelynemrg <emelynemrg@icloud.com>
2019-03-13 17:19:48 +0100

    profil 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  
   resources :events
-  resources :users
   devise_for :users
+  resources :users
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
