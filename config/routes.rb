cfe0d7649b1ac2281058a93923beadfeddb9605c
CamilleDeOliveira <36010049+CamilleDeOliveira@users.noreply.github.com>
2019-03-14 16:35:46 +0100

    design

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :events
-  root to: 'events#index'
+  get 'pages/discover'
+  root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
