d4c7a8a3a095461b36738bdf33e9334f5f3ce0cd
emelynemrg <emelynemrg@icloud.com>
2019-03-15 00:53:37 +0100

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :events
   resources  :profile
-  root to: 'events#index'
   get 'pages/discover'
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
