70d04681140c793aa0de6caa3f1bbe5333733686
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2020-02-27 17:05:10 +0100

    ordre events passés + boutons modifier ses events quand connecté + alignment el cartes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_161941) do
+ActiveRecord::Schema.define(version: 2020_02_27_144659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_161941) do
     t.string "rue"
     t.integer "code_postal"
     t.string "ville"
+    t.string "type"
   end
 
   create_table "users", force: :cascade do |t|
