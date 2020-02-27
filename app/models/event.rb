6817d65035c40eb33b8c15338dd8ec49c0d7a41d
Vanina Vizc <vanina.idiart@mmibordeaux.com>
2020-02-27 17:43:53 +0100

    EventPage & TypeList

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
-class Event < ApplicationRecord
+class Event < ActiveRecord::Base
+    belongs_to:orgaization, :class_name => 'events', :foreign_key => 'event_id'
+    CLASS_LIST = ["Bar", "Café", "Parc", "Restaurant","Sport","Théâtre"]
 end
