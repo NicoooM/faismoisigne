9d6ed24e90113b73f1fcd4fecafcf55cc85156c0
Vanina Vizc <vanina.idiart@mmibordeaux.com>
2020-02-27 18:32:50 +0100

    Home Page review

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-    belongs_to:orgaization, :class_name => 'events', :foreign_key => 'event_id'
+    belongs_to :orgaization, :class_name => 'events', :foreign_key => 'event_id'
     CLASS_LIST = ["Bar", "Café", "Parc", "Restaurant","Sport","Théâtre"]
 end
