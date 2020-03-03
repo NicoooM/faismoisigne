0fdfcd11a0d0587686a20a08873c9a7b21bb8af8
Vanina Vizc <vanina.idiart@mmibordeaux.com>
2020-03-04 00:56:26 +0100

    admin space

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    class Ability
+      include CanCan::Ability
+    
+      def initialize(user)
+        can :read, :all 
+        if user.present? 
+          can :manage, Event, user_id: user.id,
+          if user.admin?
+            can :manage, :all
+          end
+        end
+      end
+    end
+  end
+end
+
--- app/models/only_authors_authorization.rb
+++
@@ -1,5 +0,0 @@
-class OnlyAdminsAuthorization < ActiveAdmin::AuthorizationAdapter
-    def authorized?(action, subject = nil)
-      user.admin?
-    end
-  end
