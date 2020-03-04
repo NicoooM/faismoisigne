88fdf27dabb6767e9a9389fca81d8f7e53d8f9a3
Vanina Vizc <vanina.idiart@mmibordeaux.com>
2020-03-04 02:42:26 +0100

    ability conditions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,8 +11,12 @@ class Ability
         can :read, :all 
         if user.present? 
           can :manage, Event, user_id: user.id,
+          can :participate, Event do |event|
+            event.user_id != user.id
+          end
           if user.admin?
             can :manage, :all
+            cannot :participate, Event, user_id: user.id
           end
         end
       end
