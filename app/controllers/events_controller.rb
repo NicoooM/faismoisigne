981e2b5e266a8ef1baea93af496b8d712b6fb6d2
Vanina Vizc <vanina.idiart@mmibordeaux.com>
2020-03-10 23:15:05 +0100

    inscription ok

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  
   load_and_authorize_resource
 
   # GET /events
