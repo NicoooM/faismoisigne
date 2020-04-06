65ceec661e7e038fa682357b8aaeef39c0e2c4ac
Vanina Vizc <vanina.idiart@mmibordeaux.com>
2020-04-06 17:35:56 +0200

    onlineevent #1

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :events
   get 'pages/about' => 'pages#about', as: :about
   get 'pages/secret' => 'pages#secret', as: :secret
+  get 'pages/onlineevent' => 'pages#onlineevent', as: :onlineevent
   root to: 'pages#index'
 end
